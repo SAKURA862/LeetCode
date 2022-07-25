aeaf04938f7624d700ef5e784411a1f9f84b3e30
SAKURA <1772796423@qq.com>
2022-07-25 15:18:57 +0800

    数组、二分搜索

--- src/二分查找_704/Main.java
+++ src/二分查找_704/Main.java
@@ -4,10 +4,10 @@ class Solution {
     public int search(int[] nums, int t) {
         int l = 0, r = nums.length - 1;
         while (l <= r){
-            int mid = l + r + 1 >> 1;
+            int mid = (l + r) >> 1;
             if(nums[mid] == t) return mid;
             else if(nums[mid] > t) r = mid - 1;
-            else if(nums[mid] < t) l = mid + 1;
+            else l = mid + 1;
         }
         return -1;
     }
@@ -15,6 +15,7 @@ class Solution {
 
 public class Main {
     public static void main(String[] args) {
-
+        Solution solution = new Solution();
+        System.out.println(solution.search(new int[]{5}, 5));
     }
 }
