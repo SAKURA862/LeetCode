76834af18df5268263c94871a3fe5bab45068cb3
SAKURA <1772796423@qq.com>
2022-07-25 14:42:11 +0800

    哈希表、有序哈希表

--- src/剑指Offer_50_第一个只出现一次的字符/Main.java
+++ src/剑指Offer_50_第一个只出现一次的字符/Main.java
@@ -1,12 +1,17 @@
 package 剑指Offer_50_第一个只出现一次的字符;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 class Solution {
     public char firstUniqChar(String s) {
+        Map<Character, Boolean> map = new LinkedHashMap<>();
         char[] cs = s.toCharArray();
-        char[] al = new char[26];
         for(char c : cs){
-            if(al[c - 'a'] == c) return c;
-            al[c - 'a'] = c;
+            map.put(c, !map.containsKey(c));
+        }
+        for(Map.Entry<Character, Boolean> entry : map.entrySet()){
+            if(entry.getValue()) return entry.getKey();
         }
         return ' ';
     }
