9cf9a90473b14691aa3abfe6894b070aecc9a188
SAKURA <1772796423@qq.com>
2022-07-27 20:08:27 +0800

    链表、双指针

---
+++ src/面试题_02_07_链表相交/Main.java
@@ -0,0 +1,27 @@
+package 面试题_02_07_链表相交;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode A = headA, B = headB;
+        while (A != B) {
+            A = A != null ? A.next : headB;
+            B = B != null ? B.next : headA;
+        }
+        return A;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
