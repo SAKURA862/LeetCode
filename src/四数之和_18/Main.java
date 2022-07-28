965210bead43ab4dff9f2f6c38f064b0f714906a
SAKURA <1772796423@qq.com>
2022-07-28 12:43:41 +0800

    双指针 (18. 四数之和 新增不用set去重版本,完善int溢出问题)

--- src/四数之和_18/Main.java
+++ src/四数之和_18/Main.java
@@ -15,7 +15,7 @@ class Solution {
                 if(j > i + 1 && nums[j] == nums[j - 1]) continue;
                 int l = j + 1, r = n - 1;
                 while (l < r){
-                    int sum = nums[i] + nums[j] + nums[l] + nums[r];
+                    long sum = (long) nums[i] + nums[j] + nums[l] + nums[r];
                     if(sum < target) l++;
                     else if(sum > target) r--;
                     else{
@@ -33,13 +33,43 @@ class Solution {
     }
 }
 
+class Solution2 {
+    List<List<Integer>> ans = new ArrayList<>();
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        int n = nums.length;
+        if(n < 4) return new ArrayList<>();
+        Arrays.sort(nums);
+
+        for(int i = 0; i < n; i++){
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+            for(int j = i + 1; j < n; j++){
+                if(j > i + 1 && nums[j] == nums[j - 1]) continue;
+                int l = j + 1, r = n - 1;
+                while (l < r){
+                    long sum = (long) nums[i] + nums[j] + nums[l] + nums[r];
+                    if(sum < target) l++;
+                    else if(sum > target) r--;
+                    else{
+                        ans.add(Arrays.asList(nums[i], nums[j], nums[l], nums[r]));
+                        while (l < r && nums[l] == nums[l + 1]) l++;
+                        while (l < r && nums[r] == nums[r - 1]) r--;
+                        l++; r--;
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+}
+
 /**
  * @author roxy
  */
 public class Main {
     public static void main(String[] args) {
-        Solution solution = new Solution();
-        int[] nums = new int[]{-2,-1,0,0,1,2};
-        solution.fourSum(nums, 0);
+        Solution2 solution = new Solution2();
+        int[] nums = new int[]{1000000000,1000000000,1000000000,1000000000};
+        System.out.println(solution.fourSum(nums, -294967296));
     }
 }
