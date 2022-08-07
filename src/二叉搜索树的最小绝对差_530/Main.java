b0dd89b76a4c58886cf57ff4d1af361d9c27fcd2
SAKURA <1772796423@qq.com>
2022-08-07 19:03:23 +0800

    二叉搜索树、递归

---
+++ src/二叉搜索树的最小绝对差_530/Main.java
@@ -0,0 +1,42 @@
+package 二叉搜索树的最小绝对差_530;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    TreeNode pre;// 记录上一个遍历的结点
+    int result = Integer.MAX_VALUE;
+    public int getMinimumDifference(TreeNode root) {
+        if(root==null)return 0;
+        traversal(root);
+        return result;
+    }
+    public void traversal(TreeNode root){
+        if(root==null)return;
+        //左
+        traversal(root.left);
+        //中
+        if(pre!=null){
+            result = Math.min(result,root.val-pre.val);
+        }
+        pre = root;
+        //右
+        traversal(root.right);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
