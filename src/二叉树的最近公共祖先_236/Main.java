27dc594b95f58d832f4786b3b20120be1ce1c8e2
SAKURA <1772796423@qq.com>
2022-08-08 09:14:42 +0800

    二叉树、dfs、先序遍历

--- src/二叉树的最近公共祖先_236/Main.java
+++ src/二叉树的最近公共祖先_236/Main.java
@@ -25,21 +25,14 @@ class TreeNode {
 class Solution {
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null) return null;
-        if(root == p || root == q) return root;
-        if(find(root.left, p) && find(root.left, q)){
-            return lowestCommonAncestor(root.left, p, q);
+        if(root == null || root == p || root == q){
+            return root;
         }
-        if(find(root.right, p) && find(root.right, q)){
-            return lowestCommonAncestor(root.right, p, q);
-        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if(left == null) return right;
+        if(right == null) return left;
         return root;
-    }
-
-    private boolean find(TreeNode root, TreeNode node) {
-        if(root == null) return false;
-        if(root == node) return true;
-        return (find(root.left, node) || find(root.right, node));
     }
 }
 
